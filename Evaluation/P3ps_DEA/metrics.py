be72833be176d1d99140dcf054684ef2d7891c6e
wq2581 <49905511+wq2581@users.noreply.github.com>
2025-05-22 19:25:39 -0400

    Update metrics.py

--- Evaluation/P3ps_DEA/metrics.py
+++ Evaluation/P3ps_DEA/metrics.py
@@ -2,18 +2,18 @@ import pandas as pd
 from bert_score import score
 import torch
 
-# 读取CSV文件
+
 df = pd.read_csv('/blue/qsong1/wang.qing/LightRAG-main/Evaluation/P3ps_DEA/P3ps_fuzzy.csv')
 
-# 提取'disease'和'tail_disease_entName'列
+
 disease_list = df['disease'].tolist()
 tail_disease_list = df['condition'].tolist()
 
-# 检查是否有可用的GPU
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f"Using device: {device}")
 
-# 计算BERTScore并求平均
+
 def compute_bertscore_average(disease_list, tail_disease_list, device):
     precision, recall, f1 = score(disease_list, tail_disease_list, lang="en", device=device, verbose=True)
     avg_precision = precision.mean().item()
